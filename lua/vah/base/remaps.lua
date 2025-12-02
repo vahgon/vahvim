d2caa69d245c5cf832eb87ac600bff92d02b7fdb
Ian Mullins <ian.mullins.business@gmail.com>
2025-12-02 16:19:40 -0500

    fix: visual mode movement keymaps working

--- lua/vah/base/remaps.lua
+++ lua/vah/base/remaps.lua
@@ -1,14 +1,14 @@
 --lock cursor to middle of screen
-vim.keymap.set('n', '<leader>cl', function() vim.opt.scrolloff = 999 - vim.o.scrolloff end, { noremap = true, desc = "Lock cursor to middle of terminal" })
+vim.keymap.set('n', '<leader>cl', function() vim.opt.scrolloff = 999 - vim.o.scrolloff end, { silent = true, noremap = true, desc = "Lock cursor to middle of terminal" })
 
 --text editing
-vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv'", { noremap = true, desc = "Move selected text down" })
-vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv'", { noremap = true, desc = "Move selected text up" })
+vim.keymap.set('v', 'J', ":<C-U>m '>+1<CR>gv=gv", { silent = true, noremap = true, desc = "Move selected text down" })
+vim.keymap.set('v', 'K', ":<C-U>m '<-2<CR>gv=gv", { silent = true, noremap = true, desc = "Move selected text up" })
 vim.keymap.set('n', '<leader>c', '”_c', { noremap = true, desc = 'Send changed contents to "black hole" register' })
 vim.keymap.set('n', '<leader>C', '”_C', { noremap = true, desc = 'Send changed contents to "black hole" register' })
 
 --file actions
-vim.keymap.set('n', '<leader>w', '<cmd>w<cr>', { noremap = true, desc = "Save file" })
-vim.keymap.set('n', '<leader>q', '<cmd>wq<cr>', { silent = true, noremap = true, desc = "Save file and close" })
-vim.keymap.set('n', '<leader>x', '<cmd>q!<cr>', { silent = true, noremap = true, desc = "Close without saving" })
-vim.keymap.set('n', '<leader>Q', '<cmd>qa<cr>', { silent = true, noremap = true, desc = "Close all buffers" })
+vim.keymap.set('n', '<leader>w', '<cmd>w<CR>', { noremap = true, desc = "Save file" })
+vim.keymap.set('n', '<leader>q', '<cmd>wq<CR>', { silent = true, noremap = true, desc = "Save file and close" })
+vim.keymap.set('n', '<leader>x', '<cmd>q!<CR>', { silent = true, noremap = true, desc = "Close without saving" })
+vim.keymap.set('n', '<leader>Q', '<cmd>qa<CR>', { silent = true, noremap = true, desc = "Close all buffers" })
