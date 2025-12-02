9f59194337b56cd3fd5be891d7c28a17f9a2d268
Ian Mullins <ian.mullins.business@gmail.com>
2025-12-02 16:29:49 -0500

    fix: fix quote specifying black hole register

--- lua/vah/base/remaps.lua
+++ lua/vah/base/remaps.lua
@@ -1,11 +1,11 @@
---lock cursor to middle of screen
+--(c)ursor(l)ock to middle of screen
 vim.keymap.set('n', '<leader>cl', function() vim.opt.scrolloff = 999 - vim.o.scrolloff end, { silent = true, noremap = true, desc = "Lock cursor to middle of terminal" })
 
 --text editing
 vim.keymap.set('v', 'J', ":<C-U>m '>+1<CR>gv=gv", { silent = true, noremap = true, desc = "Move selected text down" })
 vim.keymap.set('v', 'K', ":<C-U>m '<-2<CR>gv=gv", { silent = true, noremap = true, desc = "Move selected text up" })
-vim.keymap.set('n', '<leader>c', '”_c', { noremap = true, desc = 'Send changed contents to "black hole" register' })
-vim.keymap.set('n', '<leader>C', '”_C', { noremap = true, desc = 'Send changed contents to "black hole" register' })
+vim.keymap.set('n', '<leader>c', '"_c', { remap = true, desc = 'Send changed contents to "black hole" register' })
+vim.keymap.set('n', '<leader>C', '"_C', { remap = true, desc = 'Send changed contents to "black hole" register' })
 
 --file actions
 vim.keymap.set('n', '<leader>w', '<cmd>w<CR>', { noremap = true, desc = "Save file" })
