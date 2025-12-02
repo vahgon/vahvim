10dfefa638980c6d70e54237e188f8edc6b0390b
Ian Mullins <ian.mullins.business@gmail.com>
2025-12-02 15:28:04 -0500

    add clang lsp

--- lua/vah/plugins/lsp/mason.lua
+++ lua/vah/plugins/lsp/mason.lua
@@ -2,7 +2,7 @@ return {
     'williamboman/mason-lspconfig.nvim',
     opts = {
         ensure_installed = {
-            'lua_ls',
+            'lua_ls', 'clangd',
         },
     },
     dependencies = {
