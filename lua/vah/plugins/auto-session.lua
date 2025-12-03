8ce47cfa5e5ee82999275dd30b87f11cde29077a
Ian Mullins <ian.mullins.business@gmail.com>
2025-12-02 20:04:45 -0500

    disable session auto-saves

--- lua/vah/plugins/auto-session.lua
+++ lua/vah/plugins/auto-session.lua
@@ -6,13 +6,16 @@ return {
         { '<leader>sr', '<cmd>AutoSession restore<CR>', desc = 'Restore session' },
         { '<leader>sf', '<cmd>AutoSession search<CR>', desc = 'Search sessions' },
         { '<leader>sa', '<cmd>AutoSession toggle<CR>', desc = 'Toggle autosave' },
+        { '<leader>sd', '<cmd>AutoSession delete', desc = 'Delete session' },
     },
 
     opts = {
     suppressed_dirs = { "~/", "/" },
     },
     config = function()
-        require('auto-session').setup({})
+        require('auto-session').setup({
+            auto_save = false,
+        })
         vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
     end,
 }
