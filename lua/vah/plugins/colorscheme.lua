cc5451a3709104b86c47c7f9ca9d5f31bdcdbcea
Ian Mullins <ian.mullins.business@gmail.com>
2025-12-02 20:04:27 -0500

    change colorscheme

--- lua/vah/plugins/colorscheme.lua
+++ lua/vah/plugins/colorscheme.lua
@@ -1,7 +1,8 @@
 return {
-	"rose-pine/neovim",
-	name = "rose-pine",
+	'catppuccin/nvim', 
+	name = 'catppuccin-mocha',
+	priority = 1000,
 	config = function()
-		vim.cmd("colorscheme rose-pine")
+		vim.cmd.colorscheme 'catppuccin-mocha'
 	end,
 }
