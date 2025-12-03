79757ce6903cc77b69a89f21f1c5e7831dc235fe
Ian Mullins <ian.mullins.business@gmail.com>
2025-12-03 14:40:22 -0500

    add folke/which-key

---
+++ lua/vah/plugins/which-key.lua
@@ -0,0 +1,15 @@
+_G.whichkeyStatus = { enabled = true }
+return {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = { delay = 1500 },
+    keys = {
+        {
+        "<leader>?",
+        function()
+            require("which-key").show({ global = false })
+        end,
+        desc = "Buffer Local Keymaps (which-key)",
+        },
+    },
+}
