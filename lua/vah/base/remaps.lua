876cb3e98c3c8d38ea942594f942bf6158dcc633
vah <ian.mullins@outlook.com>
2025-12-05 11:15:21 -0500

    added keymap that sets scrolloff = 0

--- lua/vah/base/remaps.lua
+++ lua/vah/base/remaps.lua
@@ -1,11 +1,16 @@
 --(c)ursor(l)ock to middle of screen
 vim.keymap.set('n', '<leader>cl', function() vim.opt.scrolloff = 999 - vim.o.scrolloff end, { silent = true, noremap = true, desc = "Lock cursor to middle of terminal" })
+vim.keymap.set('n', '<leader>co', function() 
+  if (vim.o.scrolloff == 10) or (vim.o.scrolloff == 0) then
+    vim.opt.scrolloff = 10 - vim.o.scrolloff 
+  end
+end, { silent = true, noremap = true, desc = "Set min lines above cursor to default/0" }) -- 10 - scrolloff because 10 is our default value set in opts.lua
 
 --text editing
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { silent = true, noremap = true, desc = "Move selected text up" })
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { silent = true, noremap = true, desc = "Move selected text down" })
-vim.keymap.set('n', '<leader>c', '"_c', { remap = true, desc = 'Send changed contents to "black hole" register' })
-vim.keymap.set('n', '<leader>C', '"_C', { remap = true, desc = 'Send changed contents to "black hole" register' })
+vim.keymap.set('n', '<leader>c', '"_c', { silent = true, noremap = true, desc = 'Send changed contents to "black hole" register' })
+vim.keymap.set('n', '<leader>C', '"_C', { silent = true, noremap = true, desc = 'Send changed contents to "black hole" register' })
 
 --file actions
 vim.keymap.set('n', '<leader>w', '<cmd>w<CR>', { noremap = true, desc = "Save file" })
