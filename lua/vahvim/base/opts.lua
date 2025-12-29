28e129336292713b5f20968b4b13447d6ca100b2
vahgon <mullins.ian@outlook.com>
2025-12-28 20:01:25 -0500

    line wrap true

--- lua/vahvim/base/opts.lua
+++ lua/vahvim/base/opts.lua
@@ -2,7 +2,7 @@ vim.opt.termguicolors = true
 vim.opt.relativenumber = true
 vim.opt.number = true
 vim.opt.cursorline = true
-vim.opt.wrap = false
+vim.opt.wrap = true
 vim.opt.signcolumn = "yes"
 
 vim.opt.tabstop = 4
