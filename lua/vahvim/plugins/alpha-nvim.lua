42cf93950aaf8b0ed315082e8aa7a45c41af0988
vahgon <mullins.ian@outlook.com>
2026-01-11 22:25:21 -0500

    change alpha dashboard btns & logos

--- lua/vahvim/plugins/alpha-nvim.lua
+++ lua/vahvim/plugins/alpha-nvim.lua
@@ -1,6 +1,5 @@
 local alpha = require('alpha')
 local dashboard = require('alpha.themes.dashboard')
-
 local logos = require("vahvim.base.logos")
 
 math.randomseed(os.time())
@@ -10,9 +9,26 @@ dashboard.section.header.val = logos[randIndex]
 dashboard.section.buttons.val = {
   dashboard.button('e', '󰄛  New File', '<cmd>ene <CR>'),
   dashboard.button('<leader>ff', '  Find file', '<cmd>Telescope find_files <CR>'),
-  dashboard.button('<leader>fg', '  Find word(s)', '<cmd>Telescope live_grep <CR>'),
   dashboard.button('q', '  Quit', '<cmd>q <CR>'),
 }
 
-alpha.setup(dashboard.opts)
+vim.api.nvim_set_hl(0, 'alphafoot', { fg = '#FF69B4', italic = true })
+
+dashboard.section.footer.val = 'vahvim'
+dashboard.section.footer.opts = {
+  position = 'center',
+  hl = 'alphafoot'
+
+}
+
+dashboard.config.layout = {
+  { type = "padding", val = 5 },
+  dashboard.section.header,
+  { type = "padding", val = 3 },
+  dashboard.section.buttons,
+  { type = "padding", val = 1 },
+  dashboard.section.footer
+}
+
+alpha.setup(dashboard.config)
 vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
