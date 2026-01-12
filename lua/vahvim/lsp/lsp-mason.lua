09b6b7aa4631d337d5272d821d5f10afdff59475
vahgon <mullins.ian@outlook.com>
2026-01-11 22:25:46 -0500

    py lsps

--- lua/vahvim/lsp/lsp-mason.lua
+++ lua/vahvim/lsp/lsp-mason.lua
@@ -1,3 +1,3 @@
 require('mason-lspconfig').setup({
-  ensure_installed = { 'lua_ls', 'clangd', "vtsls", "neocmake", "bashls", },
+  ensure_installed = { 'lua_ls', 'clangd', "vtsls", "neocmake", "bashls", "ruff", "basedpyright", },
 })
