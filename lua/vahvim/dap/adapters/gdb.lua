23e6db7623507d3827bccc65dd6ff6c87f7e6e34
vahgon <mullins.ian@outlook.com>
2026-01-16 23:16:06 -0500

    fix  eval

--- lua/vahvim/dap/adapters/gdb.lua
+++ lua/vahvim/dap/adapters/gdb.lua
@@ -2,7 +2,7 @@ require('dap').adapters.cppdbg = {
   id = 'cppdbg',
   type = 'executable',
   -- path to vscode cpptools
-  command = '$HOME/Tools/extensions/cpptools/extension/debugAdapters/bin/OpenDebugAD7',
+  command = os.getenv("HOME") .. '/Tools/extensions/cpptools/extension/debugAdapters/bin/OpenDebugAD7',
 }
 
 -- require('dap').adapters.gdb = {
