53cbcd597963888a6018aa6726a4ab64bfb9713b
vahgon <mullins.ian@outlook.com>
2026-01-19 13:29:46 -0500

    add descriptions to custom orgmode mappings

--- lua/vahvim/plugins/nvim-org.lua
+++ lua/vahvim/plugins/nvim-org.lua
@@ -1,11 +1,11 @@
 require('orgmode').setup({
   org_agenda_files = '~/Documents/org/**/*',
-  org_default_notes_file = '~/Documents/org/refile.org'
+  org_default_notes_file = '~/Documents/org/refile.org',
 })
 
 local telescope = require('telescope')
 telescope.load_extension('orgmode')
 
-vim.keymap.set('n', '<leader>fo', telescope.extensions.orgmode.refile_heading, { desc = '' })
-vim.keymap.set('n', '<leader>fh', telescope.extensions.orgmode.search_headings, { desc = '' })
-vim.keymap.set('n', '<leader>fi', telescope.extensions.orgmode.insert_link, { desc = '' })
+vim.keymap.set('n', '<leader>fo', telescope.extensions.orgmode.refile_heading, { desc = 'telescope orgmode refile heading' })
+vim.keymap.set('n', '<leader>fh', telescope.extensions.orgmode.search_headings, { desc = 'telescope orgmode search headings' })
+vim.keymap.set('n', '<leader>fi', telescope.extensions.orgmode.insert_link, { desc = 'telescope orgmode insert link' })
--- lua/vahvim/plugins/org-roam.lua
+++ lua/vahvim/plugins/org-roam.lua
@@ -1,5 +1,4 @@
 require('org-roam').setup({
   directory = "~/Documents/org/.org_roam_files",
   org_files = { "~/Documents/org/*.org" },
-
 })
