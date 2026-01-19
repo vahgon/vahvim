23aa3a88105c4eea99eee9d7b697f6f29753c3e0
vahgon <mullins.ian@outlook.com>
2026-01-19 13:27:58 -0500

    add custom mappings for nvim-tree using the api

--- lua/vahvim/plugins/tree.lua
+++ lua/vahvim/plugins/tree.lua
@@ -1,16 +1,27 @@
 local nvimtree = require('nvim-tree')
+
+local function my_on_attach(bufnr)
+  local api = require('nvim-tree.api')
+
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  api.config.mappings.default_on_attach(bufnr)
+  vim.keymap.set('n', '<C-h>', api.tree.change_root_to_parent, opts('Up'))
+  vim.keymap.set('n', '<C-l>', api.tree.change_root_to_node, opts('Down ?:o'))
+end
+
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 nvimtree.setup({
   view = {
-  width = 35,
-  relativenumber = true,
+    width = 35,
+    relativenumber = true,
   },
   renderer = {
-    indent_markers = {
-      enable = true,
-    },
+    indent_markers = { enable = true, },
     icons = {
       glyphs = {
         folder = {
@@ -27,15 +38,9 @@ nvimtree.setup({
       },
     },
   },
-  filters = {
-    custom = { '.DS_Store' },
-  },
-  git = {
-    ignore = false,
-  },
+  on_attach = my_on_attach
 })
 
-vim.keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Open Ex" })
-vim.keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Open Ex at current file" })
-vim.keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse Ex" })
+vim.keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "open nvim tree" })
+vim.keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "open ex at current file" })
 vim.keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh Ex" })
