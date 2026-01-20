340f7d21724648b7f47d1f400db2751c296e8dbd
vahgon <mullins.ian@outlook.com>
2026-01-20 14:04:31 -0500

    filter *.vim files in nvim-tree

--- lua/vahvim/plugins/tree.lua
+++ lua/vahvim/plugins/tree.lua
@@ -2,14 +2,13 @@ local nvimtree = require('nvim-tree')
 
 local function my_on_attach(bufnr)
   local api = require('nvim-tree.api')
-
   local function opts(desc)
     return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
   end
 
   api.config.mappings.default_on_attach(bufnr)
-  vim.keymap.set('n', '<C-h>', api.tree.change_root_to_parent, opts('Up'))
-  vim.keymap.set('n', '<C-l>', api.tree.change_root_to_node, opts('Down ?:o'))
+  vim.keymap.set('n', '<C-h>', api.tree.change_root_to_parent, opts('up'))
+  vim.keymap.set('n', '<C-l>', api.tree.change_root_to_node, opts('down ?:o'))
 end
 
 vim.g.loaded_netrw = 1
@@ -38,9 +37,12 @@ nvimtree.setup({
       },
     },
   },
+  filters = {
+    custom = { "\\.vim", },
+  },
   on_attach = my_on_attach
 })
 
-vim.keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "open nvim tree" })
-vim.keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "open ex at current file" })
-vim.keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh Ex" })
+vim.keymap.set('n', '<leader>ee', '<cmd>NvimTreeToggle<CR>', { desc = "open nvim tree" })
+vim.keymap.set('n', '<leader>ef', '<cmd>NvimTreeFindFileToggle<CR>', { desc = "open ex at current file" })
+vim.keymap.set('n', '<leader>er', '<cmd>NvimTreeRefresh<CR>', { desc = "refresh ex" })
