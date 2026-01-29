be60417241d8de35e41e3d2a8067570f92c43c00
vahgon <mullins.ian@outlook.com>
2026-01-29 11:57:56 -0500

    entering and exiting zen mode will additionally toggle gitsigns on/off

--- lua/vahvim/plugins/zen.lua
+++ lua/vahvim/plugins/zen.lua
@@ -1,18 +1,21 @@
-require("zen-mode").setup({
+local g = require('gitsigns')
+require('zen-mode').setup({
   on_open = function(win)
     if vim.diagnostic.is_enabled() then
       vim.diagnostic.enable(false)
+      g.toggle_signs(false)
     end
   end,
   on_close = function()
     if not vim.diagnostic.is_enabled() then
       vim.diagnostic.enable(true)
+    g.toggle_signs(true)
     end
   end,
 })
 
 vim.keymap.set('n', '<leader>z', function() require("zen-mode").toggle({
   window = {
-    width = .60 -- width will be 85% of the editor width
+    width = .60
   }
 }) end, { desc = "toggle zenmode" })
