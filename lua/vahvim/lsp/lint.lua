8caa6a59f192fefabe34b6bc414c6f72219bff0f
vahgon <mullins.ian@outlook.com>
2026-04-05 12:22:54 -0400

    changed lint mod

--- lua/vahvim/lsp/lint.lua
+++ lua/vahvim/lsp/lint.lua
@@ -1,9 +1,10 @@
+require('lint').linters_by_ft = {
+  python = { 'pylint' },
+  cpp = { 'cpplint' },
+}
+
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-  callback = function()
-    require('lint').linters_by_ft = {
-      python = { 'pylint' },
-      cpp = { 'cpplint' },
-    }
+  callback=function()
     require('lint').try_lint()
   end,
 })
