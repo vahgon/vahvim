a2d786f9a46960c580f08391990c2fe43396f683
vahgon <mullins.ian@outlook.com>
2026-06-22 15:59:43 -0400

    remove old commented out setup function

--- lua/vahvim/plugins/zen.lua
+++ lua/vahvim/plugins/zen.lua
@@ -1,19 +1,3 @@
-local g = require('gitsigns')
--- require('zen-mode').setup({
---   on_open = function(win)
---     if vim.diagnostic.is_enabled() then
---       vim.diagnostic.enable(false)
---       g.toggle_signs(false)
---     end
---   end,
---   on_close = function()
---     if not vim.diagnostic.is_enabled() then
---       vim.diagnostic.enable(true)
---     g.toggle_signs(true)
---     end
---   end,
--- })
-
 vim.keymap.set('n', '<leader>z', function() require("zen-mode").toggle({
   window = {
     width = .70
